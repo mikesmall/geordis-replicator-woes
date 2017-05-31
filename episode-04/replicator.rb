a6c7d94f4ba7bc2f96de38b8851ba33b8862bb7a
mikesmall <michael.l.a.small@gmail.com>
2017-05-31 14:02:06 -0400

    No idea about episode 4.

--- episode-04/replicator.rb
+++ episode-04/replicator.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Replicator
 
   attr_reader :plate
@@ -50,7 +52,11 @@ class Replicator
   def adjust_temperature
     return unless glass_in_tummy
 
+    # binding.pry
+
     glass_in_reactor_core = @enterprise.transporter.energize(obj: glass_in_tummy, from: @tummy, to: @enterprise.reactor.core)
+
+    # binding.pry
 
     desired_temperature = @recipe.temperature
     maximum_adjustments_allowed = 50
@@ -66,6 +72,8 @@ class Replicator
       end
 
       number_of_adjustments += 1
+
+      # binding.pry 
 
     end
 
